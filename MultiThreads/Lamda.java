d4dd7dd7d3fe4ecc9a57311614075a6254177e26
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-10 18:34:30 +0530

    Create Lamda.java

---
+++ MultiThreads/Lamda.java
@@ -0,0 +1,13 @@
+package MultiThreads;
+
+class Lamda {
+	public static void main(String args[])
+	{
+
+        new Thread(() -> {
+            System.out.println("New thread created");
+        }).start();
+		
+	}
+}
+
