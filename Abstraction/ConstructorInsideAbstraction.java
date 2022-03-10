83e3ce28530d3d31ed7fd62b0c841ed73f2c6bb3
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-10 13:13:41 +0530

    Update ConstructorInsideAbstraction.java

--- Abstraction/ConstructorInsideAbstraction.java
+++ Abstraction/ConstructorInsideAbstraction.java
@@ -1,5 +1,28 @@
 package Abstraction;
 
+abstract class Parent{
+    Parent(){
+        System.out.println("Parent Constructor called");
+    }
+
+    abstract void print();
+}
+
+class Child extends Parent{
+    Child()
+    {
+        System.out.println("Child Constructor called");
+    }
+
+    void print()
+    {
+        System.out.println("Child Print() called");
+    }
+}
+
 public class ConstructorInsideAbstraction {
-    
+    public static void main(String[] args) {
+        Child c=new Child();
+        c.print();
+    }
 }
