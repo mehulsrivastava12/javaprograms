d6d7e514781500bb48ed99d6db012fd401ce0a9b
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-10 12:08:02 +0530

    Update Test.java

--- Abstraction/Test.java
+++ Abstraction/Test.java
@@ -1,6 +1,19 @@
 package Abstraction;
 
+abstract class Base{
+    abstract void fun();
+}
+
+class Derived extends Base{
+    void fun(){
+        System.out.println("Derived fun() called");
+    }
+}
 
 public class Test {
-    
+    public static void main(String[] args) {
+        Base b=new Derived();
+        b.fun();
+    }
+
 }
