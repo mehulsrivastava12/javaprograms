ad9fe289f2c25b8f6ccb9a0bfb8868f9da33fe3f
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-11 16:58:44 +0530

    Create RemoveWord.java

---
+++ EdabitHardLevelChallenges/RemoveWord.java
@@ -0,0 +1,23 @@
+package EdabitHardLevelChallenges;
+
+import java.util.Scanner;
+
+public class RemoveWord {
+    public static String remove(String Sentence, String word){
+        if(Sentence.contains(word)){
+            String temp=word +" ";
+            Sentence=Sentence.replaceAll(temp,"");
+            temp=" " +word;
+            Sentence=Sentence.replaceAll(temp, "");
+        }
+        return Sentence;
+    }
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("ENter the Sentence");
+        String p=s.nextLine();
+        System.out.println("Enter the word to remove");
+        String q=s.nextLine();
+        System.out.println(remove(p,q));
+    }   
+}
