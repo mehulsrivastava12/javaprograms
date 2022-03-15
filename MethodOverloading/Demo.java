11e7b96cf7d5fb14cb353b9ddef468f958c2b3af
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-15 10:55:33 +0530

    Create Demo.java

---
+++ MethodOverloading/Demo.java
@@ -0,0 +1,16 @@
+package MethodOverloading;
+
+public class Demo {
+    public void show(int x){
+        System.out.println("In Int " +x);
+    }
+
+    public void show(String s){
+        System.out.println("In String " +s);
+    }
+
+    public void show(byte b){
+        System.out.println("In Byte " +b);
+    }
+
+}
