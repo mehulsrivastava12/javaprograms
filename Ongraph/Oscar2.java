760920eb676e1b988f43ebeae7c90fcdc782385c
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-21 19:57:22 +0530

    Update Oscar2.java

--- Ongraph/Oscar2.java
+++ Ongraph/Oscar2.java
@@ -7,28 +7,25 @@ public class Oscar2 {
     public static void main(String[] args){
         String file ="C:\\Users\\DELL\\Desktop\\oscar_age_male.csv";
         String line;
-        int year1=0,year2=0;
-        String movie1="",movie2="";
-        String name1="",name2="";
-        int max=0,i=0;
-        int min=44;
+        String movie1="",movie2="",name1="",name2="";
+        int max=0,i=0,min=44,year1=0,year2=0;
         HashMap<String,Integer> hm=new HashMap<String,Integer>();
         try (BufferedReader br =new BufferedReader(new FileReader(file))) {
             while((line = br.readLine()) != null){
                 String key = (line.split(","))[4];
                 if(i!=0){ 
-                if(Integer.parseInt(key)>max){
-                    max=Integer.parseInt(key);
-                    name1=(line.split(","))[6];
-                    year1=Integer.parseInt((line.split(","))[2]);
-                    movie1=(line.split(","))[8];
-                }
-                else if(Integer.parseInt(key)<min){
-                    min=Integer.parseInt(key);
-                    name2=(line.split(","))[6];
-                    year2=Integer.parseInt((line.split(","))[2]);
-                    movie2=(line.split(","))[8];
-                } 
+                    if(Integer.parseInt(key)>max){
+                        max=Integer.parseInt(key);
+                        name1=(line.split(","))[6];
+                        year1=Integer.parseInt((line.split(","))[2]);
+                        movie1=(line.split(","))[8];
+                    }
+                    else if(Integer.parseInt(key)<min){
+                        min=Integer.parseInt(key);
+                        name2=(line.split(","))[6];
+                        year2=Integer.parseInt((line.split(","))[2]);
+                        movie2=(line.split(","))[8];
+                    } 
                 }
                 else{
                     i=i+1;
