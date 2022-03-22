9a6664094a4f2fddb2c07278d74caf38861f4dfc
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-22 15:41:50 +0530

    Update LinkedHashMap1.java

--- Collections/Map/LinkedHashMap1.java
+++ Collections/Map/LinkedHashMap1.java
@@ -11,57 +11,57 @@ public class LinkedHashMap1 {
         String file="C:\\Users\\DELL\\Desktop\\cities.csv";
         String line;
         int i=0;
-        LinkedHashMap<Integer,String> hm=new LinkedHashMap<Integer,String>();
-        LinkedHashMap<Integer,String> hm2=new LinkedHashMap<Integer,String>();
+        LinkedHashMap<Integer,String> lhm=new LinkedHashMap<Integer,String>();
+        LinkedHashMap<Integer,String> lhm2=new LinkedHashMap<Integer,String>();
         try(BufferedReader br=new BufferedReader(new FileReader(file))){
             while((line=br.readLine())!=null){
                 String key=(line.split(","))[9];
-                hm.put(i,key);  //put the element in the HashMap
+                lhm.put(i,key);  //put the element in the HashMap
                 i++;
             }
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        hm.remove(0); // remove element with a key using remove method
-        hm.put(1,"PH");  // Change Value using put method
-        for(Map.Entry<Integer,String> e:hm.entrySet()){  //Iterating using for each loop
+        lhm.remove(0); // remove element with a key using remove method
+        lhm.put(1,"PH");  // Change Value using put method
+        for(Map.Entry<Integer,String> e:lhm.entrySet()){  //Iterating using for each loop
             System.out.println("Key : " +e.getKey() +" Value : "+e.getValue());
         }
-        hm.clone();  // Print and display the cloned HashMap using clone() method
+        lhm.clone();  // Print and display the cloned HashMap using clone() method
 
-        hm2.putAll(hm); //Copying one hashmap to another hashmap
+        lhm2.putAll(lhm); //Copying one hashmap to another hashmap
         
-        hm2.clear(); //Clearing the full HashMao using clear clear() method
+        lhm2.clear(); //Clearing the full HashMao using clear clear() method
         
-        hm.compute(1 , (key, val) -> val.concat("OH")); //concatenate and remap the values using compute() method
+        lhm.compute(1 , (key, val) -> val.concat("OH")); //concatenate and remap the values using compute() method
        // hm2.compute(1,(key,val) -> val.concat("OH"));
-        System.out.println(hm.containsKey(1)); //checking the hashmap contains the given key or not
+        System.out.println(lhm.containsKey(1)); //checking the hashmap contains the given key or not
         //System.out.println(hm);
 
-        System.out.println(hm.containsValue("PHOH")); //Checkinh the hashmap contains the given value
+        System.out.println(lhm.containsValue("PHOH")); //Checkinh the hashmap contains the given value
         
-        System.out.println(hm.entrySet()); // Using entrySet() to get the set view
+        System.out.println(lhm.entrySet()); // Using entrySet() to get the set view
         
-        System.out.println("\n"+hm.get(2)); // Getting the value of key 2
+        System.out.println("\n"+lhm.get(2)); // Getting the value of key 2
 
-        System.out.println(hm2.isEmpty());   //Returns true if this map contains no key-value mappings.
+        System.out.println(lhm2.isEmpty());   //Returns true if this map contains no key-value mappings.
 
-        System.out.println(hm.keySet()); // Using keySet() to get the set view of keys
+        System.out.println(lhm.keySet()); // Using keySet() to get the set view of keys
 
-        System.out.println(hm.size()); //return the size of HashMap
+        System.out.println(lhm.size()); //return the size of HashMap
 
-        System.out.println(hm.values()); // Using values() to get the set view of values
+        System.out.println(lhm.values()); // Using values() to get the set view of values
 
-        System.out.println(hm.equals(hm2)); //checks the two HashMap are equal or not
+        System.out.println(lhm.equals(lhm2)); //checks the two HashMap are equal or not
 
-        System.out.println(hm.hashCode());  // Getting the hashcode value for the map
+        System.out.println(lhm.hashCode());  // Getting the hashcode value for the map
 
-        hm2.replace(1,"CH");  //Replaces the entry for the specified key only if it is currently mapped to some value.
+        lhm2.replace(1,"CH");  //Replaces the entry for the specified key only if it is currently mapped to some value.
 
-        hm2.remove(128,"OH");  //Removes the entry for the specified key only if it is currently mapped to the specified value.
+        lhm2.remove(128,"OH");  //Removes the entry for the specified key only if it is currently mapped to the specified value.
 
-        hm2.replace(127, "PA", "CP");  //Replaces the entry for the specified key only if currently mapped to the specified value.
+        lhm2.replace(127, "PA", "CP");  //Replaces the entry for the specified key only if currently mapped to the specified value.
 
         
     }
