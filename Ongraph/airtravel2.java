db1dc639c872650a44364e8064274afcf0d4b4cb
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-23 14:18:13 +0530

    Create airtravel2.java

---
+++ Ongraph/airtravel2.java
@@ -0,0 +1,30 @@
+package Ongraph;
+import java.io.*;
+public class airtravel2 {
+    public static void main(String[] args) throws IOException {
+        String n;
+        float sumof1958 =0,sumof1959=0,sumof1960=0;
+        int i=0;
+        String path = "C:\\Users\\DELL\\Desktop\\airtravel.csv";
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        while((n=br.readLine())!=null){
+            String[] arry = n.split(",");
+            if(i==0){
+                i++;
+            }
+            else{
+                if(i>12){
+                    break;
+                }  
+                i++; 
+            sumof1958 = sumof1958 +Float.parseFloat(arry[1].trim());
+            sumof1959 = sumof1959 +Float.parseFloat(arry[2].trim());
+            sumof1960 = sumof1960 +Float.parseFloat(arry[3].trim());            
+            }     
+        }
+        System.out.println("Average of Year 1958 "+sumof1958/(i-1));
+        System.out.println("Average of Year 1959 "+sumof1959/(i-1));
+        System.out.println("Average Of Year 1960 "+sumof1960/(i-1));
+        br.close();
+    }
+}
