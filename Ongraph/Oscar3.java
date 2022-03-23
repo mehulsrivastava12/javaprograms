14f944e1b3a1f2ded22ff7aea12c6a5501c11b60
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-23 10:58:42 +0530

    Update Oscar3.java

--- Ongraph/Oscar3.java
+++ Ongraph/Oscar3.java
@@ -6,26 +6,20 @@ public class Oscar3 {
     public static void main(String[] args){
         String file ="C:\\Users\\DELL\\Desktop\\oscar_age_male.csv";
         String line;
-        int i=0;
-        TreeMap<String,String> ts=new TreeMap<String, String>();
+        TreeMap<Integer,String[]> ts=new TreeMap<Integer, String[]>();
         try (BufferedReader br =new BufferedReader(new FileReader(file))) {
+            line=br.readLine();
             while((line = br.readLine()) != null){
-                if(i!=0){
-                    String age = (line.split(","))[4];
-                    ts.put(age,line);
-                }
-                else{
-                    i=i+1;
-                    continue;
-                }
+                    String[] data =line.split(",");
+                    ts.put(Integer.parseInt(data[4]),data);
             }
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        String[] ss=ts.get(ts.firstKey()).split(",,");
-        String[] st=ts.get(ts.lastKey()).split(",,");
-        System.out.println("Youngest : \n"+ss[1]+","+ss[3]+","+ss[4]);
-        System.out.println("Eldest : \n"+st[1]+","+st[3]+","+st[4]);
+        String[] ss=ts.get(ts.firstKey());
+        String[] st=ts.get(ts.lastKey());
+        System.out.println("Youngest : \n"+ss[2]+","+ss[6]+","+ss[8]);
+        System.out.println("Eldest : \n"+st[2]+","+st[6]+","+st[8]);
     }
 }
