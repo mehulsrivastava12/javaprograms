d315b58913a62f346c4d7a9f5e3fcfdda718da20
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-23 14:20:01 +0530

    Update airtravel.java

--- Ongraph/airtravel.java
+++ Ongraph/airtravel.java
@@ -2,13 +2,13 @@ package Ongraph;
 import java.io.*;
 public class airtravel {
     public static void main(String[] args) throws IOException {
-        int sumof1958 =0,sumof1959=0,sumof1960=0,i=0,row=0;
+        String n;
+        float sumof1958 =0,sumof1959=0,sumof1960=0;
+        int i=0;
         String path = "C:\\Users\\DELL\\Desktop\\airtravel.csv";
-        String line;
         BufferedReader br = new BufferedReader(new FileReader(path));
-        while((line=br.readLine())!=null){
-            row++;
-            String[] arry = line.split(",");
+        while((n=br.readLine())!=null){
+            String[] arry = n.split(",");
             if(i==0){
                 i++;
             }
@@ -17,15 +17,14 @@ public class airtravel {
                     break;
                 }  
                 i++; 
-            sumof1958 = sumof1958 +Integer.parseInt(arry[1].trim());
-            sumof1959 = sumof1959 +Integer.parseInt(arry[2].trim());
-            sumof1960 = sumof1960 +Integer.parseInt(arry[3].trim());            
-            }  
+            sumof1958 = sumof1958 +Float.parseFloat(arry[1].trim());
+            sumof1959 = sumof1959 +Float.parseFloat(arry[2].trim());
+            sumof1960 = sumof1960 +Float.parseFloat(arry[3].trim());            
+            }     
         }
-        System.out.println("Average Of Years: "+"1958"+"  "+"1959"+"  "+"1960");
-        System.out.print("                  "+sumof1958/row);
-        System.out.print("   "+sumof1959/row);
-        System.out.print("   "+sumof1960/row);
+        System.out.println("Average of Year 1958 "+sumof1958/(i-1));
+        System.out.println("Average of Year 1959 "+sumof1959/(i-1));
+        System.out.println("Average Of Year 1960 "+sumof1960/(i-1));
         br.close();
     }
 }
