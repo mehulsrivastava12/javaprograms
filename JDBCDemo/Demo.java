20c3de94267c65f949c7b1579bbfe35229eb3cb4
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-24 15:22:44 +0530

    Update Demo.java

--- JDBCDemo/Demo.java
+++ JDBCDemo/Demo.java
@@ -5,20 +5,20 @@ public class Demo {
     public static void main(String[] args) {
         String url="jdbc:mysql://localhost:3306/jdbcdemo";
         String user="root";
-        String password="Mehul@1234";
+        String password="mehul@1234";
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Load and register
 
-            Connection con=DriverManager.getConnection(url, user, password);
+            Connection con=DriverManager.getConnection(url, user, password); //Establish Connection
 
-            Statement st=con.createStatement();
+            Statement st=con.createStatement(); //Creating Statement
 
-            ResultSet rs=st.executeQuery("Select * from employee");
+            ResultSet rs=st.executeQuery("Select * from employee"); //Executing the query
 
-            while(rs.next()){
+            while(rs.next()){ //Processing the result
                 System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4));
             }
-            con.close();
+            con.close(); //Closing the conncetion
         } 
         catch (Exception e) {
             e.printStackTrace();
