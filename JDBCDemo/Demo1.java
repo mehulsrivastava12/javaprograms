8dbcae4504ac050d7833ddae150587cf1b9837b9
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-24 17:15:52 +0530

    Create Demo1.java

---
+++ JDBCDemo/Demo1.java
@@ -0,0 +1,23 @@
+package JDBCDemo;
+import java.sql.Statement;
+import java.sql.*;
+public class Demo1 {
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/jdbcdemo";
+        String user="root";
+        String password="Mehul@1234";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Load and register
+            Connection con=DriverManager.getConnection(url, user, password); //Establish Connection
+            Statement st=con.createStatement(); //Creating Statement
+            ResultSet rs=st.executeQuery("Select * from employee where EmpDept='IT'"); //Executing the query
+            while(rs.next()){ //Processing the result
+                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4));
+            }
+            con.close(); //Closing the conncetion
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
