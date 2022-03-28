6b641289968e04a0b4f1723849c2852ec1e80909
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-28 18:32:34 +0530

    Create Dbmd.java

---
+++ JDBCDemo/Dbmd.java
@@ -0,0 +1,23 @@
+package JDBCDemo;
+import java.sql.*;  
+public class Dbmd{  
+    public static void main(String args[]){ //Program using method of DatabaseMetaData
+        String url="jdbc:mysql://localhost:3306/jdbcdemo";
+        String user="root";
+        String password="Mehul@1234"; 
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(url,user,password);  
+            DatabaseMetaData dbmd=con.getMetaData();  
+            System.out.println("Driver Name: "+dbmd.getDriverName());  
+            System.out.println("Driver Version: "+dbmd.getDriverVersion());  
+            System.out.println("UserName: "+dbmd.getUserName());  
+            System.out.println("Database Product Name: "+dbmd.getDatabaseProductName());  
+            System.out.println("Database Product Version: "+dbmd.getDatabaseProductVersion());  
+            con.close();  
+        }
+        catch(Exception e){
+             System.out.println(e);
+        }  
+    }  
+}  
