063e7de16f0479c2a0006660b3b2b6d3d4419933
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-30 09:57:41 +0530

    Update Emp1.java

--- JdbcAssignment/Emp1.java
+++ JdbcAssignment/Emp1.java
@@ -10,14 +10,15 @@ public class Emp1 {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection(url, user, password);
             Statement st=con.createStatement();
-            ResultSet rs=st.executeQuery("SELECT e.dept_no,sum(salary)"+ 
+            ResultSet rs=st.executeQuery("SELECT e.dept_no,sum(salary)"+
             "from salaries s INNER JOIN (dept_emp e,dept_manager m) ON e.emp_no=s.emp_no AND m.emp_no=s.emp_no GROUP BY e.dept_no;");
             while(rs.next()){
                 tm.put(rs.getString(1),rs.getInt(2));
             }
             con.close();
-            for (Map.Entry<String, Integer> e :tm.entrySet())
-             System.out.println(e.getKey() + " "+ e.getValue());
+            for(Map.Entry<String,Integer> e:tm.entrySet()){
+                System.out.println(e.getKey()+" "+e.getValue());
+            }
         } 
         catch (Exception e) {
             e.printStackTrace();
