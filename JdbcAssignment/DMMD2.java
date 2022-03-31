a752dad7c097e374e6f711b4036c33686d229b87
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-31 15:01:55 +0530

    Update DMMD2.java

--- JdbcAssignment/DMMD2.java
+++ JdbcAssignment/DMMD2.java
@@ -1,13 +1,11 @@
 package JdbcAssignment;
 import java.sql.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 public class DMMD2 {
     public static void main(String[] args) {
         SortedMap<Long,String> ts=new TreeMap<Long,String>();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String url="jdbc:mysql://localhost:3306/employees",user="root",password="Mehul@1234";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -16,10 +14,9 @@ public class DMMD2 {
             ResultSet rs=st.executeQuery("SELECT m.dept_no,e.first_name,e.last_name,m.from_date,m.to_date "+
             "FROM dept_manager m JOIN (employees e,dept_manager) ON e.emp_no=m.emp_no GROUP BY dept_no");
             while(rs.next()){
-                Date firstDate = sdf.parse(rs.getString(4));
-                Date secondDate = sdf.parse(rs.getString(5));
-                long diffInMillies =secondDate.getTime() - firstDate.getTime();
-                long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);  
+                LocalDate date1 = LocalDate.parse(rs.getString(4));
+                LocalDate date2 = LocalDate.parse(rs.getString(5));
+                long diff = ChronoUnit.DAYS.between(date1, date2);  
                 String value=rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+diff+" days";
                 ts.put(diff,value);
             }
