899bd303c4c95eefea84086c862b76250a235070
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-03-31 15:56:07 +0530

    Update LongestServing.java

--- JdbcAssignment/LongestServing.java
+++ JdbcAssignment/LongestServing.java
@@ -1,22 +1,24 @@
 package JdbcAssignment;
-import java.sql.Statement;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.time.LocalDate;
+import java.util.*;
 import java.sql.*;
-
+import java.time.temporal.ChronoUnit;
 public class LongestServing {
     public static void main(String[] args) {
-        SortedMap<Integer,String> a=new TreeMap<Integer,String>();
+        SortedMap<Long,String> a=new TreeMap<Long,String>();
         String url="jdbc:mysql://localhost:3306/employees",user="root",password="Mehul@1234";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection(url, user, password);
             Statement st=con.createStatement();
-            ResultSet rs=st.executeQuery("select t.title,t.emp_no,datediff(e.to_date,e.from_date)"+
-            "from dept_emp e INNER JOIN (titles t) ON t.emp_no=e.emp_no;");
+            ResultSet rs=st.executeQuery("select t.title,t.emp_no,e.from_date,e.to_date "+
+            "from dept_emp e JOIN (titles t) ON t.emp_no=e.emp_no;");
             while(rs.next()){
-                String value="Title:"+rs.getString(1)+"\nemp_no: "+rs.getInt(2)+" \n"+rs.getInt(3)+" days";
-                a.put(rs.getInt(3),value);
+                LocalDate date1 = LocalDate.parse(rs.getString(3));
+                LocalDate date2 = LocalDate.parse(rs.getString(4));
+                long diff = ChronoUnit.DAYS.between(date1, date2); 
+                String value="Title : "+rs.getString(1)+"\nEmp_NO : "+rs.getInt(2)+"\nDuration : "+diff+" days";
+                a.put(diff,value);
             }                                      
             con.close();
             System.out.println(a.get(a.lastKey()));
