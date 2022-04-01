5902eb668db0ca39d074dc3f1d508a72719267ce
mehulsrivastava12 <mhsri.1212@gmail.com>
2022-04-01 15:04:04 +0530

    Update DMMD2.java

--- JdbcAssignment/DMMD2.java
+++ JdbcAssignment/DMMD2.java
@@ -1,7 +1,5 @@
 package JdbcAssignment;
 import java.sql.*;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 public class DMMD2 {
     public static void main(String[] args) {
@@ -14,10 +12,8 @@ public class DMMD2 {
             ResultSet rs=st.executeQuery("SELECT m.dept_no,e.first_name,e.last_name,m.from_date,m.to_date "+
             "FROM dept_manager m JOIN (employees e,dept_manager) ON e.emp_no=m.emp_no GROUP BY dept_no");
             while(rs.next()){
-                LocalDate date1 = LocalDate.parse(rs.getString(4));
-                LocalDate date2 = LocalDate.parse(rs.getString(5));
-                long diff = ChronoUnit.DAYS.between(date1, date2);  
-                String value=rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+diff+" days";
+                long diff = (rs.getDate(5)).getTime()-(rs.getDate(4)).getTime();  
+                String value=rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getDate(4)+" to "+rs.getDate(5);
                 ts.put(diff,value);
             }
             con.close();
